49114d9fc208331a901cd4a80dc6880a416b343d
Jacob Chippett <jacob.chippett@gmail.com>
2026-07-29 20:54:37 -0230

    solved P2

--- solvedProblems/2AddTwoNumbers.py
+++ solvedProblems/2AddTwoNumbers.py
@@ -11,29 +11,29 @@ class Solution:
         while tempNode:
             num1str.insert(0, tempNode.val)
             tempNode = tempNode.next
-        #print(num1str)
+        print(num1str)
         tempNode = l2
         while tempNode:
             num2str.insert(0, tempNode.val)
             tempNode = tempNode.next
-        #print(num2str)
+        print(num2str)
         num1 = int("".join(map(str, num1str)))
         num2 = int("".join(map(str, num2str)))
         sum = num1+num2
         sumStr = str(sum)
-        sumList = []
         numDigits = len(sumStr)
-        for i in range(0,numDigits):
-            #sumList.append(sumStr[numDigits-1-i])
-            if i == 0:
-                currentNode:ListNode = ListNode(sumStr[i], None)
-            elif i == numDigits - 1:
-                newNode:ListNode = ListNode(sumStr[i], currentNode)
-                currentNode = None
-            else:
-                newNode:ListNode = ListNode(sumStr[i], currentNode)
-                currentNode:ListNode = ListNode(sumStr[i+1], None)
-        #print(sumList)
+        if numDigits == 1:
+            newNode = ListNode(int(sumStr[0]), None)
+        else:
+            for i in range(0,numDigits):
+                if i == 0:  #first digit
+                    newNode = ListNode(int(sumStr[i]), None)
+                    tempNode = newNode
+                elif i == numDigits - 1:    #last digit
+                    newNode = ListNode(int(sumStr[i]), tempNode)
+                else:   #other digits
+                    newNode = ListNode(int(sumStr[i]), tempNode)
+                    tempNode = newNode
         return newNode
 
 
