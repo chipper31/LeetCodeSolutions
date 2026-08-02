8945c15ce263d9f72048d0749a347f6ccb8e8f68
Jacob Chippett <jacob.chippett@gmail.com>
2026-08-02 16:56:13 -0230

    solved P509

---
+++ solvedProblems/509FibonacciNumber.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {}
+        return self.helper(n, memo)
+
+    def helper(self, n, memo):
+
+        #base cases
+        if n < 1:
+            return 0
+        if n == 1:
+            return 1
+        
+        if n in memo:
+            return memo[n]
+
+        memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+
+        return memo[n]
+        
